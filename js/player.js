5bd7ee62d04bfd1bdd901299bf16fa48c86c4c04
Sergey Lisakonov <lisakonov@ukr.net>
2016-05-17 21:59:19 +0300

    Time
    Added clickable progress bar and video time

--- js/player.js
+++ js/player.js
@@ -1,13 +1,17 @@
 window.addEventListener('load', function () {
   video = document.getElementById('video');
 
+  pbarContainer = document.getElementById('pbar-container');
   pbar = document.getElementById('pbar');
 
   playButton = document.getElementById('play-button');
+  timeField = document.getElementById('time-field');
 
   video.load();
   video.addEventListener('canplay', function () {
       playButton.addEventListener('click', playOrPause, false);
+      pbarContainer.addEventListener('click', skip, false);
+      updatePlayer();
   },false);
 }, false);
 
@@ -26,8 +30,29 @@ function playOrPause () {
 function updatePlayer () {
   var percentage = (video.currentTime/video.duration)*100;
   pbar.style.width = percentage + '%';
+  timeField.innerHTML = getFormattedTime();
   if (video.ended) {
     window.clearInterval(update);
     playButton.src = 'images/replay.png';
   }
+}
+
+function skip (ev) {
+  var mouseX = ev.pageX - pbarContainer.offsetLeft;
+  var width = window.getComputedStyle(pbarContainer).getPropertyValue('width');
+  width = parseFloat(width.substr(0, width.length - 2));
+  video.currentTime = (mouseX/width)*video.duration;
+  updatePlayer();
+}
+
+function getFormattedTime () {
+  var seconds = Math.round(video.currentTime)
+  var minutes = Math.floor(seconds / 60);
+  if (minutes > 0) seconds -= minutes * 60;
+  if (seconds.toString().length === 1) seconds = '0' + seconds;
+  var totalSeconds = Math.round(video.duration);
+  var totalMinutes = Math.floor(totalSeconds / 60);
+  if(totalMinutes > 0) totalSeconds -= totalMinutes * 60;
+  if(totalSeconds.toString().length === 1) totalSeconds = '0' + totalSeconds;
+  return minutes + ':' + seconds + ' / ' + totalMinutes + ':' + totalSeconds;
 }
